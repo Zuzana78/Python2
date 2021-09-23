388c319ea3a27e5f45516f72394e872d008d850c
Zuzana78 <53873178+Zuzana78@users.noreply.github.com>
2021-09-23 21:53:49 +0200

    Add files via upload

---
+++ lekce1_ukol2.py
@@ -0,0 +1,16 @@
+import pandas
+import requests
+import numpy
+
+r = requests.get("https://raw.githubusercontent.com/pesikj/progr2-python/master/data/london_merged.csv")
+open("london_merged.csv", 'wb').write(r.content)
+
+df_london = pandas.read_csv("london_merged.csv")
+df_london["timestamp"] = pandas.to_datetime(df_london["timestamp"])
+df_london["year"] = df_london["timestamp"].dt.year
+
+df_london_pivot = pandas.pivot_table(df_london, values="cnt", index="weather_code", columns="year", aggfunc=numpy.sum)
+
+print(df_london_pivot)
+
+
