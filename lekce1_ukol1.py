b1c684ee21557339bcfd4037d95061ecc7a84d19
Zuzana78 <53873178+Zuzana78@users.noreply.github.com>
2021-09-30 20:30:42 +0200

    Update lekce1_ukol1.py

--- lekce1_ukol1.py
+++ lekce1_ukol1.py
@@ -3,10 +3,10 @@ import requests
 import numpy
 
 r = requests.get("https://raw.githubusercontent.com/pesikj/progr2-python/master/data/titanic.csv")
-open("tested.csv", 'wb').write(r.content)
+open("titanic.csv", 'wb').write(r.content)
 
 df_titanic = pandas.read_csv("titanic.csv")
-df_titanic_pivot = pandas.pivot_table(df_titanic, values="Survived", index="Pclass", columns="Sex", aggfunc=numpy.sum)
+df_titanic_pivot = pandas.pivot_table(df_titanic, values="Survived", index="Pclass", columns="Sex", aggfunc=numpy.sum, margins=True)
 #pandas.set_option('display.max_columns', None)
 print(df_titanic_pivot)
 
